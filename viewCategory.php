e16ffbb7f9f0a8d1e043a365fc3a5e1e7c7d8e15
Elefant04 <58483574+Elefant04@users.noreply.github.com>
2022-03-13 08:54:10 +0100

    basic db interaction

--- viewCategory.php
+++ viewCategory.php
@@ -1,9 +1,22 @@
 <?php 
+header('Content-Type: text/html; charset=utf-8');
+if(!isset($_SESSION)){
+	session_start();
+}
 require_once('inc/parser.php');
 include_once('lang/de.php');
+require_once('inc/db_queries.php');
+
+if(isset($_GET['category'])){
+	$dynamicReplacementArray  = array('results' => getProductsFromCategory($_GET['category']));
+} else {
+	//output Error 404
+	include('index.php');
+	exit;
+}
 
 
-$parsedOutput = parseHTML('suchseite.html',$langReplacementArray);
+$parsedOutput = parseHTML('suchseite.html',$langReplacementArray, $dynamicReplacementArray);
 
 foreach($parsedOutput as $line){
 	echo $line;
