a68929dd1ad3a9cc29c0a3a46d2ca5fee31f815d
Elefant04 <58483574+Elefant04@users.noreply.github.com>
2022-03-13 12:46:39 +0100

    real products

--- inc/db_queries.php
+++ inc/db_queries.php
@@ -6,7 +6,7 @@
 
 	function getAllProducts(){
 		global $db;
-		$query = "SELECT *, p.name as produktName FROM produkt p JOIN hersteller h ON p.hersteller = h.id JOIN kategorie k ON p.kategorie = k.id";
+		$query = "SELECT *, p.name as produktName, FORMAT((p.preis_normal * (1 - p.rabatt/100)),2) as preis FROM produkt p JOIN hersteller h ON p.hersteller = h.id JOIN kategorie k ON p.kategorie = k.id";
 		try{
 			$prepStat = $db -> prepare($query);
 			$prepStat -> execute();
@@ -22,7 +22,7 @@
 
 	function getMostExpensiveProducts(){
 		global $db;
-		$query = "SELECT *, p.name as produktName FROM produkt p JOIN hersteller h ON p.hersteller = h.id JOIN kategorie k ON p.kategorie = k.id ORDER BY (p.preis_normal * (1 - p.rabatt/100)) DESC LIMIT 5";
+		$query = "SELECT *, p.name as produktName, FORMAT((p.preis_normal * (1 - p.rabatt/100)),2) as preis FROM produkt p JOIN hersteller h ON p.hersteller = h.id JOIN kategorie k ON p.kategorie = k.id ORDER BY (p.preis_normal * (1 - p.rabatt/100)) DESC LIMIT 5";
 		try{
 			$prepStat = $db -> prepare($query);
 			$prepStat -> execute();
@@ -38,7 +38,7 @@
 
 	function getProductsFromCategory($kategorie){
 		global $db;
-		$query = "SELECT *, p.name as produktName FROM produkt p JOIN hersteller h ON p.hersteller = h.id JOIN kategorie k ON p.kategorie = k.id WHERE p.kategorie = :kategorieId";
+		$query = "SELECT *, p.name as produktName, FORMAT((p.preis_normal * (1 - p.rabatt/100)),2) as preis FROM produkt p JOIN hersteller h ON p.hersteller = h.id JOIN kategorie k ON p.kategorie = k.id WHERE p.kategorie = :kategorieId";
 		try{
 			$prepStat = $db -> prepare($query);
 			$prepStat -> bindParam(':kategorieId', $kategorie);
