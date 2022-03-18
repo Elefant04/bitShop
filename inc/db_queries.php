524141e7b631bfcf9100424fea347a5b78fb3556
Elefant04 <58483574+Elefant04@users.noreply.github.com>
2022-03-18 11:38:14 +0100

    AJAX

--- inc/db_queries.php
+++ inc/db_queries.php
@@ -75,4 +75,60 @@
 		return $prepStat -> fetchAll();
 	}
 
+
+	function getShortProductsFromSearchQuery($SearchQuery){
+		global $db;
+		$query = "SELECT p.bild as bild, p.name as produktName, FORMAT((p.preis_normal * (1 - p.rabatt/100)),2) as preis FROM produkt p WHERE p.name LIKE :query LIMIT 5";
+		try{
+			$prepStat = $db -> prepare($query);
+			$prepStat -> bindValue(':query', '%' . $SearchQuery . '%');
+			$prepStat -> execute();
+		} catch (PDOException $e) {
+			echo '<p>Verbindung fehlgeschlagen!</p>';
+			if(ini_get('display_errors')){
+				echo '<p>' . $e -> getMessage();
+			}
+			exit;
+		}
+		return $prepStat -> fetchAll();
+	}
+
+	function getProductsFromSearchQuery($SearchQuery){
+		global $db;
+		$query = "SELECT *, p.name as produktName, FORMAT((p.preis_normal * (1 - p.rabatt/100)),2) as preis  FROM produkt p WHERE p.name LIKE :query LIMIT 10";
+		try{
+			$prepStat = $db -> prepare($query);
+			$prepStat -> bindValue(':query', '%' . $SearchQuery . '%');
+			$prepStat -> execute();
+		} catch (PDOException $e) {
+			echo '<p>Verbindung fehlgeschlagen!</p>';
+			if(ini_get('display_errors')){
+				echo '<p>' . $e -> getMessage();
+			}
+			exit;
+		}
+		return $prepStat -> fetchAll();
+	}
+
+	function getProductsFromRangeOfSearchQuery($SearchQuery, $start, $rowcount){
+		global $db;
+		if(!(is_numeric($start) && is_numeric($rowcount))){
+			$start = 0;
+			$rowcount = 10;
+		}
+		$query = "SELECT *, p.name as produktName, FORMAT((p.preis_normal * (1 - p.rabatt/100)),2) as preis  FROM produkt p WHERE p.name LIKE :query LIMIT $start, $rowcount";
+		try{
+			$prepStat = $db -> prepare($query);
+			$prepStat -> bindValue(':query', '%' . $SearchQuery . '%');
+			$prepStat -> execute();
+		} catch (PDOException $e) {
+			echo '<p>Verbindung fehlgeschlagen!</p>';
+			if(ini_get('display_errors')){
+				echo '<p>' . $e -> getMessage();
+			}
+			exit;
+		}
+		return $prepStat -> fetchAll();
+	}
+
 ?>
