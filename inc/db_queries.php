9ffcd51ba8b507b50942b6aa79c32c5ad252ad78
Elefant04 <58483574+Elefant04@users.noreply.github.com>
2022-03-18 09:54:43 +0100

    prepare

--- inc/db_queries.php
+++ inc/db_queries.php
@@ -53,4 +53,26 @@
 		return $prepStat -> fetchAll();
 	}
 
+
+	function getRangeOfProductsFromCategory($kategorie, $start, $rowcount){
+		global $db;
+		if(!(is_numeric($start) && is_numeric($rowcount))){
+			$start = 0;
+			$rowcount = 10;
+		}
+		$query = "SELECT *, p.name as produktName, FORMAT((p.preis_normal * (1 - p.rabatt/100)),2) as preis FROM produkt p JOIN hersteller h ON p.hersteller = h.id JOIN kategorie k ON p.kategorie = k.id WHERE p.kategorie = :kategorieId  LIMIT $start, $rowcount";
+		try{
+			$prepStat = $db -> prepare($query);
+			$prepStat -> bindParam(':kategorieId', $kategorie);
+			$prepStat -> execute();
+		} catch (PDOException $e) {
+			echo '<p>Verbindung fehlgeschlagen!</p>';
+			if(ini_get('display_errors')){
+				echo '<p>' . $e -> getMessage();
+			}
+			exit;
+		}
+		return $prepStat -> fetchAll();
+	}
+
 ?>
